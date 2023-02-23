dc59a083a868c38813ff63643ddc897650103465
Zaire Ali <zaireali649@gmail.com>
2023-02-23 16:51:27 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def web_server():
     html = ''
     html += '<h1>Changed and Updated</h1>\n'
     html += "<p>Hello, World!</p>"
