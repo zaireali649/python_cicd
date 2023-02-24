af437320ddfae962e0033103a263305d3c2cc457
Zaire Ali <zaireali649@gmail.com>
2023-02-23 20:27:31 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def web_server():
-    html = ''
-    html += '<h1>Changed and Updated</h1>\n'
-    html += "<p>Hello, World!</p>"
+    html = '''
+    <h1>Flask Hosted Website</h1>
+    <p>This is a CICD example of running a pipeline using Python. Please like and subscribe!</p>
+    <h2>https://github.com/zaireali649/python_cicd</h2>
+    '''
+    
     return html
+
